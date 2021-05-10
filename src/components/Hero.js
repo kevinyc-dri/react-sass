24f755ec8a114f0b6b0ae680693e30b716fc8c06
Kevin Choi <kevinyoungchoi@gmail.com>
2021-05-10 13:34:31 -0400

    added bootstrap and bootstrap components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import {Carousel} from 'react-bootstrap'
+import slider1 from './../assets/slider1.jpg'
+import slider2 from './../assets/slider2.jpg'
+import slider3 from './../assets/slider3.jpg'
 
 function Hero() {
   return (
     <div className='hero'>
-      <h1>this is a hero</h1>
+      <Carousel>
+        <Carousel.Item className='hero__item'>
+          <img
+            className="d-block w-100"
+            src={slider1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={slider2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={slider3}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
 
     </div>
   )
